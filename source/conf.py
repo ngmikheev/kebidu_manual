08152378b220a89711eaeb763b2dafd0be5c3c8f
mikheev <ng.mikheev@physics.msu.ru>
2024-01-07 23:13:15 +0300

    HTML theme switched to read the docs

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,8 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_parser"
+    "myst_parser",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ['_templates']
@@ -26,5 +27,5 @@ language = 'ru'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
